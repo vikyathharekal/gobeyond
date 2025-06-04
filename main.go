21d9f78d198ddee38428766d51819ab4d167ffe4
Deepak Bhatt <19406111+deepakbhatt329@users.noreply.github.com>
2025-06-04 10:45:05 +0530

    test

--- main.go
+++ main.go
@@ -5,14 +5,13 @@ import (
 	_ "os" // import for side-effects or to keep lint warning
 )
 
+var unusedVariable = 42 // declared but not used
+
 func main() {
 	message := "Hello, World!"
 	PrintMessage(message)
-	_ = unusedVariable // unused variable
 }
 
 func PrintMessage(msg string) { // exported function without comment
 	fmt.Println(msg)
 }
-
-var unusedVariable = 42 // declared but not used
